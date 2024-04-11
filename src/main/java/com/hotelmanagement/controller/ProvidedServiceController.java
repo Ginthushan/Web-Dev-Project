69c7945398f4bba9badacfb59c59b3794c9da8ad
michaelcultrera <michael.cultrera@ontariotechu.net>
2024-04-11 14:17:18 -0400

    remove import

--- src/main/java/com/hotelmanagement/controller/ProvidedServiceController.java
+++ src/main/java/com/hotelmanagement/controller/ProvidedServiceController.java
@@ -3,13 +3,10 @@ package com.hotelmanagement.controller;
 import com.hotelmanagement.model.ProvidedService;
 import com.hotelmanagement.service.ProvidedServiceService;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @Controller
 public class ProvidedServiceController {
